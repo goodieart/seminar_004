fa1e0cec8f5fa6ee94ad465218223b8ff81d326a
Pavel Yastremsky <goodieart@gmail.com>
2022-10-03 06:38:30 +0300

    task_027 completed

---
+++ task_027/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+Write($"Сумма цифр числа = {digit_sum(number)}");
+
+int digit_sum(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
