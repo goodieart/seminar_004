c653d71a984077d8dd157ea28053c43d38d75d41
Pavel Yastremsky <goodieart@gmail.com>
2022-10-03 11:32:18 +0300

    task_025 fixed

--- task_025/Program.cs
+++ task_025/Program.cs
@@ -6,14 +6,11 @@ float a = float.Parse(ReadLine()!);
 Write("Введите степень B: ");
 int b = int.Parse(ReadLine()!);
 
-Write($"A^B = {pow(a,b)}");
+Write($"A^B = {pow(a,b):f2}");
 
 float pow(float number, int power)
 {
     float result = 1;
-    for(int i = 0; i < power; i++)
-    {
-        result *= number;
-    }
+    for(int i = 0; i < power; i++) result *= number;
     return result;
 }
