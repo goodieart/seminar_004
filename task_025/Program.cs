ffc4549c98acf8e7999e40d3ad52a6488cf5d9c2
Pavel Yastremsky <goodieart@gmail.com>
2022-10-03 06:27:30 +0300

    task_025 completed

---
+++ task_025/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+WriteLine("Введите A");
+float a = float.Parse(ReadLine()!);
+
+WriteLine("Введите B");
+int b = int.Parse(ReadLine()!);
+
+Write($"A^B = {pow(a,b)}");
+
+float pow(float number, int power)
+{
+    float result = 1;
+    for(int i = 0; i < power; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
