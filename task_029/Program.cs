a1571d98ae2ed69d9002b5e5a84f4f195c0c116f
Pavel Yastremsky <goodieart@gmail.com>
2022-10-03 06:56:10 +0300

    task_029 completed

---
+++ task_029/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+int[] array = array_create();
+
+int[] array_create()
+{
+    int[] result = new int[8];
+    Write("[");
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(0,100);
+        Write($"{result[i]},");
+    }
+    Write("\b]");
+    return result;
+}
